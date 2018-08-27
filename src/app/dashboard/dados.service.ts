68b6ced772caca97ad04c9254ef1ef7ec31d8b28
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-27 16:52:33 -0300

    Implementando service
    Implementando o service de dados

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DadosService {
 
-  constructor() { }
+	readonly dados = [
+        ['Janeiro', 33],
+        ['Fevereiro', 68],
+        ['Março', 49],
+        ['Abril', 15],
+        ['Maio', 80],
+        ['Junho', 27],
+        ['Julho', 76],
+        ['Agosto', 12],
+        ['Setembro', 104],
+        ['Outubro', 99],
+        ['Novembro', 27],
+        ['Desembro', 35],
+    ]
+
+	constructor() {}
+
+	/**
+	 * Retorna um observable contendo os dados a serem
+	 * exibidos no gráfico.
+	 *
+	 * @return Observable<any>
+	 */
+	obterDados(): Observable<any> {
+		return new Observable(observable => {
+			observable.next(this.dados);
+			observable.complete();
+		});
+	}
 }
